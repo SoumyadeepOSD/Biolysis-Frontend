c6bb1548c725dfbf429f479b60accfbabcaf26b0
SoumyadeepOSD <opencontainer255@gmail.com>
2024-09-06 02:23:42 +0530

    route.ts fixed

--- app/api/general-response/route.ts
+++ app/api/general-response/route.ts
@@ -3,8 +3,8 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(request:NextRequest){
     const query = request.nextUrl.searchParams.get("query");
-    // const API_URI = process.env.NEXT_API_URL!;
-    const API_URI = "http://localhost:8000";
+    const API_URI = process.env.NEXT_API_URL!;
+    // const API_URI = "http://localhost:8000";
     try {
         const response = await axios.get(`${API_URI}/interface/v1/response/general?query=${query}`);
         return NextResponse.json({data: response.data, status: 200});
